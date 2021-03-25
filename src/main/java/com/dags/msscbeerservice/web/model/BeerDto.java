539e1005c0d3d7a6ad9bed79fc9dc5952ee10ce6
Chris <christopher-dags@hotmail.com>
2021-03-24 22:23:16 -0400

    Added Jackson configuration for BeerDto

--- src/main/java/com/dags/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/dags/msscbeerservice/web/model/BeerDto.java
@@ -26,6 +26,7 @@ public class BeerDto {
     @Null
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
+
     @Null
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
