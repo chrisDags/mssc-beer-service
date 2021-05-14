816265a9e8d50f86ca54d62cc3b282a3c4826bc3
Chris <christopher-dags@hotmail.com>
2021-05-14 14:47:09 -0400

    Configured HTTP Basic Security for Feign Client, closes #11

--- src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableFeignClients
 @SpringBootApplication
